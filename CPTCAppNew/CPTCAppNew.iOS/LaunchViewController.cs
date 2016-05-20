97fdec6c23c461b8d9a4c055d1d9ae8c8bce317e
Drew Britton <drewbritton@gmail.com>
2016-05-20 15:25:18 -0700

    Added all LaunchScreen images, fixed a constraint.

---
+++ CPTCAppNew/CPTCAppNew.iOS/LaunchViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace CPTCAppNew.iOS
+{
+	partial class LaunchViewController : UIViewController
+	{
+		public LaunchViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
