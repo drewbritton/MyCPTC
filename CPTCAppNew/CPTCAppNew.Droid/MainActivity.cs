38ea8fdccbaf201a16b6f061928989b707f58423
Drew Britton <drewbritton@gmail.com>
2016-05-23 21:06:40 -0700

    Started designing Android UI.

--- CPTCAppNew/CPTCAppNew.Droid/MainActivity.cs
+++ CPTCAppNew/CPTCAppNew.Droid/MainActivity.cs
@@ -7,27 +7,17 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace CPTCAppNew.Droid
-{
-	[Activity (Label = "CPTCAppNew.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+namespace CPTCAppNew.Droid {
 
-		protected override void OnCreate (Bundle bundle)
-		{
+	[Activity (Label = "CPTCAppNew.Droid", MainLauncher = true)]
+	public class MainActivity : Activity {
+
+		protected override void OnCreate (Bundle bundle) {
 			base.OnCreate (bundle);
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
 		}
 	}
 }
