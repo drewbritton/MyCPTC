daf5c11fa4cde551fa24d294703cc2d11a5b739e
Drew Britton <drewbritton@gmail.com>
2016-06-01 18:11:19 -0700

    Comments added to SchoolEventsDb class.

--- CPTCApp/SchoolEventsDb.cs
+++ CPTCApp/SchoolEventsDb.cs
@@ -4,9 +4,19 @@ using SQLite;
 using CPTCAppNew;
 
 namespace CPTCApp {
-	
+
+	/// <summary>
+	/// Provides a SQLite database comprised of a SchoolEventsTable
+	/// List that contains all events from our Events Web API. Also
+	/// allows Insert/Update commands and Select count(*) queries.
+	/// </summary>
 	public class SchoolEventsDb {
-		
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CPTCApp.SchoolEventsDb"/> class.
+		/// </summary>
+		/// <param name="path">The path of the local database within the iOS/Android
+		/// filesystem.</param>
 		public SchoolEventsDb(string path) {
 			createDatabase(path);
 			var listData = GetSchoolEvents();
@@ -22,15 +32,24 @@ namespace CPTCApp {
 
 				insertUpdateData(tableData, path);
 			}
-
-			//insertUpdateData(listData, path);
 		}
 
+		/// <summary>
+		/// Makes a call to "GetEventsAsync()" from the SchoolEventsRepository
+		/// class.
+		/// </summary>
+		/// <returns>A List of SchoolEvents objects.</returns>
 		public List<SchoolEvents> GetSchoolEvents() {
 			var returnedEvents = new SchoolEventsRepository();
 			return returnedEvents.GetEventsAsync();
 		}
 
+		/// <summary>
+		/// Constructs a SQLite database locally.
+		/// </summary>
+		/// <returns>A string that signifies that the database was
+		/// created successfully.</returns>
+		/// <param name="path">The path of the local database.</param>
 		public string createDatabase(string path) {
 			try {
 				var connection = new SQLiteAsyncConnection(path);
@@ -42,6 +61,12 @@ namespace CPTCApp {
 				}
 		}
 
+		/// <summary>
+		/// Allows Insert and Update commands to the SQLite local database.
+		/// </summary>
+		/// <returns>Whether or not the data was successfully inserted/updated.</returns>
+		/// <param name="data">The SchoolEventsTable data object.</param>
+		/// <param name="path">The path of the local database.</param>
 		public string insertUpdateData(SchoolEventsTable data, string path) {
 			try {
 				var db = new SQLiteAsyncConnection(path);
@@ -55,6 +80,11 @@ namespace CPTCApp {
 			}
 		}
 
+		/// <summary>
+		/// Reports the number (count) of all database entries.
+		/// </summary>
+		/// <returns>The number of records.</returns>
+		/// <param name="path">The path of the local database.</param>
 		private int findNumberRecords(string path) {
 			try {
 				var db = new SQLiteAsyncConnection(path);
